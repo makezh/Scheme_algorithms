8afdd281704083325fac7a3f65d575b872e1c3b6
makezh <max.kezhaev@gmail.com>
2021-09-10 18:36:23 +0300

    add quation solve algorithm

--- 1_laba.rkt
+++ 1_laba.rkt
@@ -2,3 +2,13 @@
   (if (< m 3)
       (remainder (+ d (quotient (* 31 (+ m 10)) 12) (- y 1) (quotient (- y 1) 4) (- (quotient (- y 1) 100)) (quotient (- y 1) 400)) 7)
       (remainder (+ d (quotient (* 31 (- m 2)) 12) y  (quotient y 4) (- (quotient y 100)) (quotient y 400)) 7)))
+
+(define (equation a b c)
+  (define D (- (* b b) (* 4 a c)))
+  (if (< D 0)
+      (list)
+      (if (= D 0)
+          (list (/ (- b) (* 2 a)))
+          (list (/ (+ (- b) (sqrt D)) (* 2 a)) (/ (- (- b) (sqrt D)) (* 2 a)))
+      )))
+
