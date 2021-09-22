9453b04ea0a7b7db28c5b03302e4ef85d64cc3ba
makezh <max.kezhaev@gmail.com>
2021-09-22 21:13:50 +0300

    add new homework's tasks

--- homework_2.rkt
+++ homework_2.rkt
@@ -5,9 +5,12 @@
       '()
       (cons a (my-range (+ a d) b d))))
 
-(my-range  0 11 3) (newline)
 
-;---------------------------
+(display "#1") (newline)
+(display "(my-range  0 11 3) -> ")
+(my-range  0 11 3) 
+
+;------------------------
 
 (define (my-flatten xs)
   (if (null? xs)
@@ -16,9 +19,10 @@
           (append (car xs) (my-flatten (cdr xs)))
           (cons (car xs) (my-flatten (cdr xs))))))
 
-(my-flatten '(1 2 (3 4) 5 (7 8 9 10) (12)) ) (newline)
+(display "(my-flatten '(1 2 (3 4) 5 (7 8 9 10) (12))) -> ")
+(my-flatten '(1 2 (3 4) 5 (7 8 9 10) (12)) )
 
-;-----------------------------------------------------
+;------------------------
 
 (define (my-element? x xs)
   (cond
@@ -26,10 +30,62 @@
     ((equal? x (car xs)) #t)
     (else (my-element? x (cdr xs)))))
 
+(display "(my-element? 122 '(1 2 3 122 x)) -> ")
 (my-element? 122 '(1 2 3 122 x))
+(display "(my-element? 12 '(1 2 3 122 x)) -> ")
 (my-element? 12 '(1 2 3 122 x))
 
-;------------------------------
+;------------------------
 
+(define (my-filter pred? xs)
+  (if (null? xs)
+      '()
+      (if (pred? (car xs))
+          (cons (car xs) (my-filter pred? (cdr xs)))
+          (my-filter pred? (cdr xs)))))
+
+(display "(my-filter odd? (my-range 0 10 1)) -> ")
+(my-filter odd? (my-range 0 10 1))
+(display "(my-filter (lambda (x) (= (remainder x 3) 0)) (my-range 0 13 1)) -> ")
+(my-filter (lambda (x) (= (remainder x 3) 0)) (my-range 0 13 1))
+
+;------------------------
+
+;(define (my-fold-left op xs))
+
+;------------------------
+
+;(define (my-fold-right op xs))
+
+(newline)
+;=========================
+
+;#2
+
+(define (list->set xs)
+  (if (null? xs)
+      '()
+      (if (my-element? (car xs) (cdr xs))
+          (list->set (cdr xs))
+          (cons (car xs) (list->set (cdr xs))))))
+
+(display "#2") (newline)
+(display "(list->set '(1 1 1 2 3)) -> ")
+(list->set '(1 1 1 2 3))
+
+
+;=========================
+
+;#5
+
+(define (f x) (+ x 2))
+(define (g x) (* x 3))
+(define (h x) (- x))
+
+(define (o . xs)
+  (lambda (x)
+    (if (null? xs)
+        x
+        ((car xs) ((apply o (cdr xs)) x)  ))))
 
 
