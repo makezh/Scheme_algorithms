fe04200e3ef7337db5d89d5292e4c5b6b2916c3b
makezh <max.kezhaev@gmail.com>
2021-10-30 19:59:36 +0300

    add 2nd task in labwork

--- 3_laba.rkt
+++ 3_laba.rkt
@@ -1,14 +1,14 @@
 ;#1
 (define-syntax trace-ex 
   (syntax-rules () 
-    ((trace-ex object) 
-     (let ((result object)) 
+    ((trace-ex x) 
+     (let ((res x)) 
        (begin 
-         (write 'object) 
-         (display " => ") 
-         (write result) 
+         (write 'x) 
+         (write " => ") 
+         (write res) 
          (newline) 
-         result)))))
+         res)))))
 
 
 (define (zip . xss)
@@ -25,3 +25,57 @@
 ;--------------------------
 ;#2
 
+(define-syntax test
+  (syntax-rules ()
+    ((test fx x)
+     (list `fx fx x) ; функция (car) -- факт ответ (cadr) -- ожид ответ (caddr)
+     )))
+
+(define (signum x)
+  (cond
+    ((< x 0) -1)
+    ((= x 0)  1)
+    (else     1)))
+
+(define the-tests
+  (list 
+   (test (signum -2) -1)
+   (test (signum 0) 0)
+   (test (signum 2) 1)))
+
+(define (run-test t)
+  (write (car t))
+  (if (= (cadr t) (caddr t))
+      (begin (display " ok\n") #t)
+      (begin
+        (display " FAIL\n")
+        (display "  Expected: ")
+        (write (caddr t))
+        (newline)
+        (display "  Returned: ")
+        (write (cadr t))
+        (newline)
+        #f)))
+
+(define fl #t)
+(define (run-tests t)
+  (if (null? t)
+      fl
+      (if (run-test (car t))
+          (run-tests (cdr t))
+          (begin (set! fl #f) (run-tests (cdr t)))
+          )))
+
+(display "#2\n")
+(run-tests the-tests)
+(newline)
+
+;--------------------------
+      
+
+
+
+      
+      
+
+  
