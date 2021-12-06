2ccff3f78115fc8ec454baee7e1fcd11ea2240da
makezh <max.kezhaev@gmail.com>
2021-12-06 21:27:12 +0300

    add #5 in 4th labwork

--- 4_laba.scm
+++ 4_laba.scm
@@ -117,3 +117,33 @@
 
 (run-tests if-tests)
 
+;#5
+(display "\n#5\n")
+
+(define-syntax my-let
+  (syntax-rules ()
+    ((my-let ((name1 value1) ...) expr1 ...)
+     ((lambda (name1 ...) expr1 ...) value1 ...))))
+
+(define-syntax my-let*
+  (syntax-rules ()
+    ((my-let* () expr1 ...)
+     (my-let () expr1 ...))
+    ((my-let* ((name1 value1)) expr1 ...)
+     (my-let ((name1 value1)) expr1 ...))
+    ((my-let* ((name1 value1) (name2 value2) ...) expr1 ...)
+     (my-let ((name1 value1))
+             (my-let* ((name2 value2) ...) expr1 ...)))))
+
+(define let-tests
+  (list
+   (test (my-let* () 10) 10)
+   (test (my-let* ((x 100)) x) 100)
+   (test (my-let* ((x 10) (y (* x 10))) (+ x y)) 110)))
+
+(run-tests let-tests)
+
+;#6
+(display "\n#6\n")
+
+
