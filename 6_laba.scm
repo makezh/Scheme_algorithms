be6f50761ac4945da109bd7dcd76e0adaa84e7b4
makezh <max.kezhaev@gmail.com>
2021-12-21 01:22:20 +0300

    add more comments in #1 6th labwork

--- 6_laba.scm
+++ 6_laba.scm
@@ -10,8 +10,8 @@
 ;<sign> := + | -
 
 ;FINISH
-(define force-return 0)
-(define (FINISH) (force-return #f))
+(define break 0)
+(define (FINISH) (break #f))
 
 (define ie (interaction-environment))
 (define call/cc call-with-current-continuation)
@@ -48,8 +48,8 @@
                     (loop other 'denom)
                     (set-car! (cdr (assoc stage expr)) '())) ;делаем пустым словарь, если что-то не то
                 (if (char-numeric? char)
-                    (begin (set-car! (cdr (assoc stage expr)) (append (cadr (assoc stage expr)) (list char)))
-                           (loop other stage))
+                    (begin (set-car! (cdr (assoc stage expr)) (append (cadr (assoc stage expr)) (list char))) ;добавляем символ в список числителя/знаменателя
+                           (loop other stage)) 
                     (set-car! (cdr (assoc stage expr)) '()))))))
     expr))
 
@@ -60,54 +60,64 @@
          (not (null? (dict-ref expr 'denom))))))
 
 (define (scan-frac str)
-  (and (check-frac str)
-       (let ((expr (fill-frac (string->list str))))
-         (eval (list '/
+  (and (check-frac str) 
+       (let ((expr (fill-frac (string->list str)))) 
+         (eval (list '/ ;склеиваем в выражение вида: (/ +110/111)
                      (list (dict-ref expr 'sign)
                            (symbol->number (dict-ref expr 'num)))
                      (symbol->number (dict-ref expr 'denom))) ie))))
 
 (define (split-fracs str)
-  (let loop ((fracs '()) (it-frac "") (listr (string->list str)))
+  (let loop ((fracs '()) (temp-frac "") (listr (string->list str))) ;(цельные выражения -- формирующееся выражение -- строка, которая уже список
     (if (null? listr)
-        (if (> (string-length it-frac) 0)
-            (append fracs (list it-frac))
+        (if (> (string-length temp-frac) 0)
+            (append fracs (list temp-frac))
             fracs)
         (if (or (equal? (car listr) #\tab)
                 (equal? (car listr) #\newline)
                 (equal? (car listr) #\space))
-            (if (> (string-length it-frac) 0)
-                (loop (append fracs (list it-frac)) "" (cdr listr))
-                (loop fracs it-frac (cdr listr)))
-            (loop fracs (string-append it-frac (string (car listr))) (cdr listr))))))
+            (if (> (string-length temp-frac) 0)
+                (loop (append fracs (list temp-frac)) "" (cdr listr)) ;сохраняем сформированное выражение в общий список выражений
+                (loop fracs temp-frac (cdr listr)))
+            (loop fracs (string-append temp-frac (string (car listr))) (cdr listr)))))) ;добавляем в формирующееся выражение
 
 ;<content> := <frac> | <content> <frac>
 (define (scan-many-fracs str)
   (call/cc
    (lambda (stack)
-     (set! force-return stack)
-     (let loop ((str-fracs (split-fracs str)))
-       (if (null? str-fracs) '()
-           (let ((scanned-frac (scan-frac (car str-fracs))))
-             (if (not scanned-frac)
-                 (FINISH)
-                 (cons scanned-frac (loop (cdr str-fracs))))))))))
+     (set! break stack) 
+     (let loop ((str-fracs (split-fracs str))) 
+       (if (null? str-fracs)
+           (list)
+           (let ((scanned-frac (scan-frac (car str-fracs)))) ;поочередно проверяем выражения
+             (if scanned-frac ;если отсканировалось, то сохраняем, иначе - завершаем
+                 (cons scanned-frac (loop (cdr str-fracs)))
+                 (FINISH))))))))
 
 ; TESTS
 
-(check-frac "110/111")
-(check-frac "-4/3") 
-(check-frac "+5/10")
-(check-frac "5.0/10")
-(check-frac "FF/10")
 
-(scan-frac "110/111")  
-(scan-frac "-4/3")     
-(scan-frac "+5/10")    
-(scan-frac "5.0/10")   
-(scan-frac "FF/10")    
+(define tests
+  (list
+   (test (check-frac "110/111") #t)
+   (test (check-frac "-4/3") #t)    
+   (test (check-frac "+5/10") #t)   
+   (test (check-frac "5.0/10") #f)
+   (test (check-frac "FF/10") #f)
+   (test (check-frac "/313") #f)
+   (test (check-frac "-2a3/23") #f)
 
-(scan-many-fracs
- "\t1/2 1/3\n\n10/8")  
+   (test (scan-frac "110/111") 110/111)
+   (test (scan-frac "-4/3") -4/3)
+   (test (scan-frac "+5/10") 1/2)
+   (test (scan-frac "5.0/10") #f)
+   (test (scan-frac "FF/10") #f)
+
+   (test (scan-many-fracs "111/1233 11/222 -2/23") '(111/1233 11/222 -2/23))
+   (test (scan-many-fracs "\t1/2 1/3\n\n10/8") '(1/2 1/3 5/4))
+   (test (scan-many-fracs "\t1/2 1/3\n\n2/-5") #f)))
+;(run-tests tests)
+
+;(scan-many-fracs "\t1/2 1/3\n\n10/8")  
 (scan-many-fracs
  "\t1/2 1/3\n\n2/-5")  
